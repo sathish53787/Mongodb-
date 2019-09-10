fe52afbdbff233cfe2f79294921a95c22e84673f
sathiah <kaleeswaran77moorthy@gmail.com>
2019-09-10 21:21:07 +0530

    mongodb

---
+++ mongodb.js
@@ -0,0 +1,24 @@
+
+db.person.insert({
+   name  : "Sathish",
+   age   : "20",
+   skills :["node js","java script"]
+});
+
+db.person.insert({
+    name  : "ronaldo",
+    age   : "25"
+    skills :["Foot ball"]
+ });
+
+ db.person.insert({
+    name  : "messi",
+    age   : "20",
+    skills :["Foot ball"]
+ });
+
+ db.person.insert({
+    name  : "dhoni",
+    age   : "29",
+    skills :["cricket"]
+ });
